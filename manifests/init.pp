429d891b2a8a4e7daa7338a82360e80233e75b99
alexkappa <alex.kalyvitis@gmail.com>
2014-07-14 23:09:45 +0200

    Fixed comments

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,23 @@
 # == Class: aerospike
 #
-# Full description of class aerospike here.
+# Installs the Aerospike server.
 #
 # === Parameters
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*version*]
+#   The version of aerospike server to download.
 #
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+# [*download_dir*]
+#   Where to store downloaded packages.
 #
 # === Examples
 #
-#  class { aerospike: }
+#  class { aerospike:
+#   version      => "3.3.8",
+#   download_dir => "/usr/local/src",
+#  }
 #
 # === Authors
 #
